c0257744edd9389149839c2800448360f12acc02
selfhyginting <selfhyginting.iklc@gmail.com>
2022-03-24 12:25:13 +0700

    add get data and search fitur

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -10,46 +10,41 @@
       </tr>
       </thead>
       <tbody>
-      <tr v-for="item in bio" :key="item.index">
-        <td>{{item.nama}}</td>
+      <tr v-for="item in bio" :key="item.id">
+        <td>{{item.name}}</td>
         <td>{{item.email}}</td>
         <td>{{item.gender}}</td>
       </tr>
       </tbody>
     </table>
-    <button class="btn btn-sm btn-outline-danger">Delete</button>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
-import {onMounted, ref} from 'vue'
 
 export default {
   name: "about",
   data(){
     return {
-      bio : [
-        {
-          id:"1",
-          nama:"selfhy",
-          email: "s@gmail",
-          gender: "female",
-        },
-        {
-          id:"2",
-          nama:"defhy",
-          email: "d@gmail",
-          gender: "female",
-        },
-        {
-          id:"3",
-          nama:"bana",
-          email: "b@gmail",
-          gender: "male",
-        }
-      ],
+      bio : [],
     };
+  },
+  methods:{
+    setBio(data){
+      this.bio = data;
+    }
+      
+  },
+  mounted(){
+   axios.get('https://gorest.co.in/public/v2/users')
+    .then((response) => {
+      //console.log("success : ",response);
+      this.setBio(response.data);
+    })
+    .catch((error) => {
+      console.log("error : ",error);
+    });
   },
 }
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,17 @@
 <template>
-  <div class="home">
-    <h1>this is home</h1>
+  <div class="container">
+    <div class="mb-2">
+      <h1>Search Data</h1>
+      <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="search.." v-model="dataCari">
+      </div>
+    </div>
+  <!--   <button class="btn btn-sm btn-outline-primary" :onClick="searchData">Search</button>
+   --></div>
+  <div class="container mt-2">
+    <ul>
+      <li v-for="item in searchData" :key="item.index">{{item}}</li>
+    </ul>
   </div>
 </template>
 
@@ -11,6 +22,31 @@ export default {
   name: 'HomeView',
   components: {
     
+  },
+  data(){
+    return{
+      dataCari:[],
+      data : [
+        'Danawi Liam',
+        'Tarjaya',
+        'Daruna',
+        'Warsoni',
+        'John Wick',
+        'Hadi PS',
+        'Derian Lekso'
+        ]
+    }
+  },
+  methods:{
+
+  },
+  computed:{
+    searchData(){
+     return this.data.filter((item)=>{
+       return item.match(this.dataCari);
+     })
+      
+    }
   }
 }
 </script>
